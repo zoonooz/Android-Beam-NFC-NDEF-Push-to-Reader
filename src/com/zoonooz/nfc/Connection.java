6c73a024a0a312c5d0883753e70b739e653de8c2
Amornchai Kanokpullwad <amornchai.zoon@gmail.com>
2012-03-27 03:00:08 +0700

    Update String output and catch some error

--- src/com/zoonooz/nfc/Connection.java
+++ src/com/zoonooz/nfc/Connection.java
@@ -1,3 +1,23 @@
+/*
+ * Connection - Connect and receive data ACR122u 
+ * 
+ * Copyright (C) 2012  Amornchai Kanokpullwad <amornchai.zoon@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package com.zoonooz.nfc;
 
 import ds.nfcip.NFCIPUtils;
@@ -50,7 +70,15 @@ public class Connection {
                 ch = terminal.connect("*").getBasicChannel();
                 if (ch != null) {
                     initAsTarget();
-                    waitForTouchToBeam();
+                    try{
+                        waitForTouchToBeam();
+                    }catch(CardException ex){
+                        System.out.println("[Error] "+ex.getMessage());
+                        if (terminal.waitForCardAbsent(0)) {
+                            start();
+                        }
+                        
+                    }
                 } else {
                     System.out.println("[Error] no channel");
                 }
@@ -64,31 +92,37 @@ public class Connection {
     }
     
     private void initAsTarget() {
-        sendCommand(TG_INIT_AS_TARGET, TG_TARGET_PAYLOAD);
+        try {
+            System.out.println("Init as Target");
+            sendCommand(TG_INIT_AS_TARGET, TG_TARGET_PAYLOAD,false);
+        } catch (CardException ex) {
+            System.out.println("[Error] "+ex.getMessage());
+        }
     }
 
-    private void waitForTouchToBeam() {
+    private void waitForTouchToBeam() throws CardException {
         byte[] ccPayload = {(byte) 0x85, (byte) 0x81};
         byte[] response = null;
         System.out.println("Waiting for Beam");
         while (true) {
             try {
                 Thread.sleep(50);
-                response = sendCommand(TG_GET_DATA, null);
+                response = sendCommand(TG_GET_DATA, null ,false);
                 
                 if (response[3] == 0x11) {
                     System.out.println("Beam receive");
                     byte[] ccNewPayload = {(byte) 0x81, (byte) 0x84};
-                    sendCommand(TG_SET_DATA, ccNewPayload);
+                    sendCommand(TG_SET_DATA, ccNewPayload,false);
                     
-                    sendCommand(TG_GET_DATA, null);
-                    sendCommand(TG_SET_DATA, ccNewPayload);
+                    sendCommand(TG_GET_DATA, null,false);
+                    sendCommand(TG_SET_DATA, ccNewPayload,false);
                     
                     getDataFromBeam();
                     break;
                 }
                 
-                sendCommand(TG_SET_DATA, ccPayload);
+                sendCommand(TG_SET_DATA, ccPayload,false);
+            
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
@@ -96,13 +130,26 @@ public class Connection {
     }
     
     private byte[] getDataFromBeam(){
-        byte[] data = sendCommand(TG_GET_DATA, null);
-        System.out.println(new String(NFCIPUtils.subByteArray(data, 3, data.length-5)));
-        return data;
+        try {
+            byte[] data = sendCommand(TG_GET_DATA, null,true); //APDU response 
+            
+            byte[] llcp = NFCIPUtils.subByteArray(data, 3, data.length-5); //LLCP
+            byte[] snep = NFCIPUtils.subByteArray(llcp, 3, llcp.length-3); //SNEP protocol
+            byte[] ndef = NFCIPUtils.subByteArray(snep, 6, snep.length-6); //NDEF
+            byte[] ndef_text = NFCIPUtils.subByteArray(ndef, 7, ndef.length-7); //NDEF text
+            
+            
+            System.out.println(new String(ndef_text));
+            
+            return data;
+        } catch (CardException ex) {
+            System.out.println("[Error] "+ex.getMessage());
+            return null;
+        }
     }
 
-    private byte[] sendCommand(byte intruction, byte[] payload) {
-        try {
+    private byte[] sendCommand(byte intruction, byte[] payload,boolean debug) throws CardException {
+        
             int payloadLength = (payload != null) ? payload.length : 0;
             byte[] instruction = {(byte) 0xd4, intruction};
 
@@ -114,30 +161,32 @@ public class Connection {
 
             cmd = NFCIPUtils.appendToByteArray(cmd, payload);
 
-            System.out.println("[Sent]     (" + cmd.length + " bytes): " + NFCIPUtils.byteArrayToString(cmd));
+            if(debug)
+                System.out.println("[Sent]     (" + cmd.length + " bytes): " + NFCIPUtils.byteArrayToString(cmd));
 
             CommandAPDU c = new CommandAPDU(cmd);
             ResponseAPDU response = ch.transmit(c);
 
             byte[] responseByte = response.getBytes();
-            System.out.println("[Received] (" + responseByte.length + " bytes): " + NFCIPUtils.byteArrayToString(responseByte));
+            
+            
+            if(debug)
+                System.out.println("[Received] (" + responseByte.length + " bytes): " + NFCIPUtils.byteArrayToString(responseByte));
 
             if (response.getSW1() == 0x63 && response.getSW2() == 0x27) {
                 throw new CardException(
                         "wrong checksum from contactless response (0x63 0x27");
             } else if (response.getSW1() == 0x63 && response.getSW2() == 0x7f) {
                 throw new CardException("wrong PN53x command (0x63 0x7f)");
+            } else if (response.getBytes()[2] == 0x29) {
+                throw new CardException("Card remove ?");
             } else if (response.getSW1() != 0x90 && response.getSW2() != 0x00) {
                 throw new CardException("unknown error ("
                         + NFCIPUtils.byteToString(response.getSW1()) + " "
                         + NFCIPUtils.byteToString(response.getSW2()));
-            }
-
+            } 
+            //System.out.println(NFCIPUtils.byteArrayToString(response.getData()));
             return responseByte;
-        } catch (CardException ex) {
-            System.out.println("[Error] " + ex.getMessage());
-            System.exit(0);
-            return null;
-        }
+        
     }
 }
